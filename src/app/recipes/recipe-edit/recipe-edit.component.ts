bfe34c78f138731ad5bf8967d66ad429bb8c6bd1
gitgenie <gayathri.lakshman89@gmail.com>
2018-02-11 22:32:09 +0530

    211 Syncing HTML with the Form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -8,18 +10,37 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute, private recipeService:RecipeService) { }
   id:number;
   editMode = false;
+  recipeForm: FormGroup;
   
   ngOnInit() {
   	this.route.params.subscribe(
 	(params:Params)=>{
 			this.id = params['id'];
 			this.editMode = params['id']!= null;
-			//console.log(this.editMode);
+			this.initForm();
 		}	
 	);
+  }
+  
+  initForm(){
+  let recipeName ='';
+  let recipeImagePath ='';
+  let recipeDescription ='';
+  
+  if(this.editMode){
+  	const recipe = this.recipeService.getRecipe(this.id);
+	recipeName = recipe.name;
+	recipeImagePath =recipe.imagePath;
+  	recipeDescription =recipe.description;
+  }
+  	this.recipeForm = new FormGroup({
+		'name': new FormControl(recipeName),
+		'imagePath': new FormControl(recipeImagePath),
+		'description': new FormControl(recipeDescription)
+	});
   }
 
 }
