ecc91415bb537576caa1d8265acc602ec096d7ae
gitgenie <gayathri.lakshman89@gmail.com>
2018-02-16 23:52:48 +0530

    245 Transforming Response Data to Prevent Errors

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 
@@ -15,9 +16,20 @@ export class DataStorageService{
 	
 	getRecipes(){
 		this.http.get('https://ng-recipe-book-7d766.firebaseio.com/recipes.json')
-		.subscribe(
+		.map(
 			(response: Response) => {
 				const recipes : Recipe[] = response.json();
+				for(let recipe of recipes){
+					if(!recipe['ingredients']){
+					console.log(recipe);
+						recipe['ingredients'] = [];
+					}
+				}
+				return recipes;
+			}
+		)
+		.subscribe(
+			(recipes: Recipe[]) => {
 				this.recipeService.setRecipes(recipes);
 			}
 		);
