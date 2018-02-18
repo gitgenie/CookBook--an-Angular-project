94a1467987822e4910bdb29b4f9d5982972cd6d2
gitgenie <gayathri.lakshman89@gmail.com>
2018-02-18 22:32:04 +0530

    251 Signing Users Up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,8 @@
-export class AuthService(){
+import * as  firebase from 'firebase';
+export class AuthService{
  	signUp(email:string, password:string){
-	
+		firebase.auth().createUserWithEmailAndPassword(email, password).catch(
+			error => console.log(error)
+		);
 	}
 }
