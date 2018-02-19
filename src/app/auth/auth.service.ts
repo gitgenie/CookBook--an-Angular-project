bca026dcdff4d79034a0f7dfe959cae44a7a25bd
gitgenie <gayathri.lakshman89@gmail.com>
2018-02-19 23:06:26 +0530

    257 Route Protection and Redirection Example

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,12 @@
 import * as  firebase from 'firebase';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable()
 export class AuthService{
 	token: string;
+	
+	constructor(private router:Router){}
 	
  	signUp(email:string, password:string){
 		firebase.auth().createUserWithEmailAndPassword(email, password).catch(
